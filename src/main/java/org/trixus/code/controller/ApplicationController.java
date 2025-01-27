60d83b5374d213b81c6e53796770c5bfe651b077
TrixusCode <theetsoreetsang@gmail.com>
2025-01-27 09:19:54 +0200

    fixed the response of application api

--- src/main/java/org/trixus/code/controller/ApplicationController.java
+++ src/main/java/org/trixus/code/controller/ApplicationController.java
@@ -1,9 +1,10 @@
 package org.trixus.code.controller;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.trixus.code.dto.ApplicationDto;
-import org.trixus.code.model.Application;
 import org.trixus.code.model.vo.Status;
 import org.trixus.code.service.ApplicationService;
 
@@ -14,40 +15,38 @@ import java.util.Optional;
 @RequestMapping("/applications")
 public class ApplicationController {
     private final ApplicationService applicationService;
-    private ModelMapper modelMapper;
-
     public ApplicationController(ApplicationService applicationService) {
         this.applicationService = applicationService;
     }
 
     @PostMapping("/submit")
-    public ApplicationDto submitApplication (@RequestBody ApplicationDto applicationRequest) {
-        return applicationService.createApplication(applicationRequest);
+    public ResponseEntity<ApplicationDto> submitApplication (@RequestBody ApplicationDto applicationRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(applicationService.createApplication(applicationRequest));
     }
 
     @GetMapping("/submittedApplications")
-    public List<ApplicationDto> getAllApplications (){
-        return applicationService.getAllApplication();
+    public ResponseEntity<List<ApplicationDto>> getAllApplications (){
+        return ResponseEntity.status(HttpStatus.OK).body(applicationService.getAllApplication());
     }
 
     @GetMapping("/application/{status}")
-    public List<ApplicationDto> getAllApplicationsByStatus(@PathVariable("status") String status) {
-        return applicationService.getApplicationsByStatus(status);
+    public ResponseEntity<List<ApplicationDto>> getAllApplicationsByStatus (@PathVariable("status") String status) {
+        return ResponseEntity.status(HttpStatus.OK).body(applicationService.getApplicationsByStatus(status));
     }
 
     @PutMapping("/update/{id}/{status}")
-    public ApplicationDto updateApplicationByStatus(@PathVariable("id") Long id , @PathVariable("status") String status) {
-        return applicationService.updateApplicationStatus(id, Status.valueOf(status));
+    public ResponseEntity<ApplicationDto> updateApplicationByStatus (@PathVariable("id") Long id , @PathVariable("status") String status) {
+        return ResponseEntity.status(HttpStatus.OK).body(applicationService.updateApplicationStatus(id, Status.valueOf(status)));
     }
 
     @GetMapping("/{id}")
-    public Optional<ApplicationDto> getApplicationById (@PathVariable("id") Long id) {
-        return applicationService.getApplicationById(id);
+    public ResponseEntity<Optional<ApplicationDto>> getApplicationById (@PathVariable("id") Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(applicationService.getApplicationById(id));
     }
 
     @PutMapping("/updateApplication/{id}")
-    public ApplicationDto updateApplication(@RequestBody ApplicationDto application,@PathVariable("id") Long applicationId) {
-        return applicationService.updateApplication(applicationId, application);
+    public ResponseEntity<ApplicationDto> updateApplication(@RequestBody ApplicationDto application,@PathVariable("id") Long applicationId) {
+        return ResponseEntity.status(HttpStatus.OK).body(applicationService.updateApplication(applicationId, application));
     }
     @DeleteMapping("application/{id}")
     public void deleteApplication (@PathVariable("id") Long id) {
